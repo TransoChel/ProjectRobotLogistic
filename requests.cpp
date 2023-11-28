50e6ff7402082911a93b67d24376dafacf516168
Селин Андрей Сергеевич <selinas@sch239.net>
2023-11-28 20:34:09 +0300

    start codding algoritm Deykstra

--- requests.cpp
+++ requests.cpp
@@ -1,35 +1,54 @@
 #include <iostream>
 #include <vector>
+
+unsigned const int numberOfDots = 0;
+Dot dots[numberOfDots];
+
 class dot
 {
-    public:
-        std::string name;
-        int number;
-        std::vector<int[2]> ways;
-        dot(std::string name, int number)
-        {
-            this->name = name;
-            this-> number = number;
-        }
+public:
+    std::string name;
+    weight;
+    bool isUsed;
+    std::vector<int[2]> ways;
 };
 
-void addWay(dot* dot1, dot* dot2, int lenght)
+void addWay(dot *dot1, dot *dot2, int lenght)
 {
-    dot1->ways.push_back({dot2->number, lenght});
-    dot2->ways.push_back({dot1->number, lenght});
+  //  dot1->ways.push_back({dot1->name, lenght}); ПЕРЕПИСАТЬ
+  //  dot2->ways.push_back({dot1->number, lenght});
 }
 
 void sendToRobot(std::vector<int> path)
 {
-
 }
 
 class request
 {
-    public:
-        request(dot from, dot to)
-        {
-            std::vector<int> path;
-            //дописать
+public:
+    request(dot from, dot to)
+    {/*
+        std::vector<int> path;
+            void dijkstra(dot s);
+        for(int v = 0; v < numberOfDots; v++) 
+        {      
+            dots[v].weight = 9999;
+            dots[v].isUsed = false;
         }
+        from.weight = 0;
+        dot V(V, );
+        for j∈V
+                            // найдём вершину с минимальным расстоянием
+            if !used[j] and (v == null or d[j] < d[v])
+                    v = j
+            if d[v] == ∞
+
+            break
+        used[v] = true
+        for e : исходящие из v рёбра     // произведём релаксацию по всем рёбрам, исходящим из v
+            if d[v] + e.len < d[e.to]
+                d[e.to] = d[v] + e.len
+            // дописать по алгоритму дейкстры*/
+    }
 };
+
