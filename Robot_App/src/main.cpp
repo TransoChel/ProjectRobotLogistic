a55e2c7e0a2641415ba5084ad8f6b758d6b14db4
Селин Андрей Сергеевич <selinas@sch239.net>
2023-12-15 19:31:31 +0300

    modifyApp

--- Robot_App/src/main.cpp
+++ Robot_App/src/main.cpp
@@ -18,9 +18,7 @@ int main() {
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //---------------------------------------------------------------------------------------
 
-    Texture2D logo = LoadTexture("../tectures/logo.png");
-
-
+    Texture2D logo = LoadTexture("../textures/logo.png"), doRequest = LoadTexture("../textures/doRequest.png");
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -29,7 +27,7 @@ int main() {
         //----------------------------------------------------------------------------------
 
         if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-            if(CheckCollisionPointRec(GetMousePosition(), {960, 350, 500, 200}) && !OrderMenu) {
+            if(CheckCollisionPointRec(GetMousePosition(), {960, 450, 128*5, 32*5}) && !OrderMenu) {
                 OrderMenu = true;
             }
         }
@@ -50,13 +48,16 @@ int main() {
         ClearBackground(WHITE);
         DrawRectangleV({0, 100}, {500, 935}, {255, 0, 0, 255});
         DrawRectangleV({0, 0}, {screenWidth, 100}, {200, 0, 0, 255});
+        
+        DrawTextureEx(logo, {18, 18}, 0, 2, WHITE);
         DrawText("Queue", 175, 125, 50, WHITE);
         if (!OrderMenu) {
-            DrawRectangleV({960, 350}, {500, 200}, {255, 0, 0, 255});
-            DrawText("Order", 1150, 410, 50, WHITE);
+            DrawTextureEx(doRequest, {960, 450}, 0, 5, WHITE);
+            //DrawRectangleV({960, 350}, {500, 200}, {255, 0, 0, 255});
+            //DrawText("Order", 1150, 410, 50, WHITE);
         }
         else {
-            DrawText("Placeholder", 1070, 25, 50, WHITE);
+            DrawText("Customize your request", 920, 25, 50, WHITE);
             DrawRectangleV({1570, 935}, {1680, 885}, {255, 0, 0, 255});
             DrawText("To main", 1650, 955, 50, WHITE);
         }
