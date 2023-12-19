197227448601f312c13c16b21d7bf498a13c920b
matuhovat <toma-school@mail.ru>
2023-12-19 20:32:08 +0300

    app updates

--- Robot_App/src/main.cpp
+++ Robot_App/src/main.cpp
@@ -1,8 +1,10 @@
 #include <string>
-
+#include <vector>
 #include <raylib.h>
 #define RAYGUI_IMPLEMENTATION
 #include "extras/raygui.h"
+#include "../../robot/requests.cpp"
+// 255 130 130
 
 int main() {
     // Initialization
@@ -13,6 +15,11 @@ int main() {
     InitWindow(screenWidth, screenHeight, "RoboLogist");
 
     bool OrderMenu = false;
+    bool Sent = false;
+    int from, to;
+    std::vector <Request> Queue;
+    Color selectedColor = GREEN;
+    
 
     std::string text = "Ffjaslkj";
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -34,11 +41,11 @@ int main() {
             if(CheckCollisionPointRec(GetMousePosition(), {960, 450, 128*5, 32*5}) && !OrderMenu) {
                 OrderMenu = true;
             }
-        }
-        if(IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-        {
-            if(CheckCollisionPointRec(GetMousePosition(), {1420, 885, 1820, 935}) && OrderMenu)
+            else if(CheckCollisionPointRec(GetMousePosition(), {1420, 885, 1820, 935}) && OrderMenu) {
                 OrderMenu = false;
+            }
+            //else if (CheckCollisionPointRec(GetMousePosition(), ))
+            // else if (CheckCollisionPointCircle(GetMousePosition(), ))
         }
 
         //----------------------------------------------------------------------------------
@@ -55,15 +62,25 @@ int main() {
         
         DrawTextureEx(logo, {18, 18}, 0, 2, WHITE);
         DrawTextureEx(queue, {100, 125}, 0, 5, WHITE);
-        if (!OrderMenu) {
+        if (!OrderMenu && !Sent) {
             DrawTextureEx(doRequest, {960, 450}, 0, 5, WHITE);
             //DrawRectangleV({960, 350}, {500, 200}, {255, 0, 0, 255});
             //DrawText("Order", 1150, 410, 50, WHITE);
         }
         else {
-            DrawTextureEx(toMenu, {1350, 855}, 0, 5, WHITE);
-            DrawTextureEx(fromTo, {920, 125}, 0, 5, WHITE);
-            DrawTextureEx(send, {1150, 855}, 0, 5, WHITE);
+            Sent = false;
+            DrawTextureEx(toMenu, {1550, 900}, 0, 3, WHITE);
+            DrawText("From", 750, 250, 60, RED);
+            DrawRectangleV({675, 325}, {350, 75}, GRAY);
+            DrawText("A", 830, 340, 50, RED);
+            DrawRectangleV({675, 400}, {350, 75}, GRAY);
+            DrawText("B", 830, 415, 50, RED);
+            DrawRectangleV({675, 475}, {350, 75}, GRAY);
+            DrawText("C", 830, 480, 50, RED);
+            DrawRectangleV({675, 550}, {350, 75}, GRAY);
+            DrawText("D", 830, 555, 50, RED);
+            DrawTextureEx(fromTo, {920, 125}, 0, 3, WHITE);
+            DrawTextureEx(send, {1425, 900}, 0, 3, WHITE);
         }
 
         EndDrawing();
